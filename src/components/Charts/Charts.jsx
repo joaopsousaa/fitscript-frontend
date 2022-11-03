c058cfd3d4187213b2054f472c408281189936cc
joaopsousaa <104941882+joaopsousaa@users.noreply.github.com>
2022-11-03 20:59:33 +0000

    trying again

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import { Chart } from "chart.js/auto";
 
 function Charts({ chartData }) {
-  console.log(ChartJS);
+  console.log(Chart);
   return (
     <div>
       <Line
--- src/components/Charts/TotalWeightLifted.jsx
+++ src/components/Charts/TotalWeightLifted.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import { Chart } from "chart.js/auto";
 // import { useState } from "react";
 
 function TotalWeightLifted({ chartData }) {
-  console.log(ChartJS);
+  console.log(Chart);
   return (
     <div>
       <Line
