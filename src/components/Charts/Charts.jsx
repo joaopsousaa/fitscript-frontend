de011206f910a62d8bc35eeabbed4d68e52aeea6
joaopsousaa <104941882+joaopsousaa@users.noreply.github.com>
2022-11-03 20:32:52 +0000

    fix deploy error

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
+console.log(ChartJS);
+
 function Charts({ chartData }) {
   return (
     <div>
