709245af0d3f1f28c71e6b68f532a63f038fab8d
joaopsousaa <104941882+joaopsousaa@users.noreply.github.com>
2022-11-03 18:16:12 +0000

    total weight per workout chart

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,42 +1,34 @@
 import "./DashboardPage.css";
 import Charts from "../../components/Charts/Charts";
+import TotalWeightLifted from "../../components/Charts/TotalWeightLifted";
 import Loading from "../../components/Loading/Loading";
 import { useContext } from "react";
 import { UserWorkoutsContext } from "../../context/userWorkouts.context";
+import { format } from "date-fns";
 
 function DashboardPage() {
   const userWorkouts = useContext(UserWorkoutsContext);
   console.log("List of user workouts:", userWorkouts);
 
   const workoutDate = userWorkouts.map((data) => {
-    return data.createdAt;
+    return format(new Date(data.createdAt), "dd/MM/yyyy");
   });
 
   const workoutTotalTimeWorkingOut = userWorkouts.map((data) => {
     return data.totalTimeWorkingOut;
   });
 
-  // const sumWeightLifted = userWorkouts.reduce((acc, currentValue) => {
-  //   currentValue.exercises.map((exercise) => {
-  //     return exercise.sets.map((set) => {
-  //       console.log(acc);
-  //       return set.numberOfReps * set.weightLifted;
-  //     });
-  //   });
-  //   return acc + currentValue;
-  // }, 0);
-
-  const arrayOfWeightLifted = userWorkouts.map((element) =>
-    element.exercises.map((exercise) =>
+  const arrayOfWeightLifted = userWorkouts.map((workout) =>
+    workout.exercises.flatMap((exercise) =>
       exercise.sets.map((set) => set.numberOfReps * set.weightLifted)
     )
   );
 
-  const sumOfWeightLifted = arrayOfWeightLifted
-    .flat(Infinity)
-    .reduce((acc, currentValue) => acc + currentValue, 0);
+  const totalWeightLiftedPerWorkout = arrayOfWeightLifted.map((workout) =>
+    workout.reduce((acc, currentValue) => acc + currentValue, 0)
+  );
 
-  console.log("this is totalweight", sumOfWeightLifted);
+  console.log("this is totalweight", totalWeightLiftedPerWorkout);
 
   const chartData = {
     labels: workoutDate,
@@ -53,20 +45,20 @@ function DashboardPage() {
     ],
   };
 
-  // const totalWeightLifted = {
-  //   labels: workoutDate,
+  const totalWeightLiftedChartData = {
+    labels: workoutDate,
 
-  //   datasets: [
-  //     {
-  //       label: "Time Working Out",
-  //       lineTension: 0.5,
-  //       backgroundColor: "rgba(75,192,192,1)",
-  //       borderColor: "rgba(0,0,0,1)",
-  //       borderWidth: 2,
-  //       data: workoutTotalTimeWorkingOut,
-  //     },
-  //   ],
-  // };
+    datasets: [
+      {
+        label: "Total Weight Lifted Per Workout (kg)",
+        lineTension: 0.5,
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: totalWeightLiftedPerWorkout,
+      },
+    ],
+  };
 
   console.log(chartData);
 
@@ -76,7 +68,10 @@ function DashboardPage() {
       {userWorkouts.length === 0 ? (
         <Loading />
       ) : (
-        <Charts chartData={chartData} />
+        <>
+          <Charts chartData={chartData} />
+          <TotalWeightLifted chartData={totalWeightLiftedChartData} />
+        </>
       )}
     </div>
   );
