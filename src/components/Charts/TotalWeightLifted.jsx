395fb46bb0ec2331f4be2b52e658aeddfee4ada5
joaopsousaa <104941882+joaopsousaa@users.noreply.github.com>
2022-11-04 20:12:53 +0000

    save file

--- src/components/Charts/TotalWeightLifted.jsx
+++ src/components/Charts/TotalWeightLifted.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { Chart } from "chart.js";
+import Chart from "chart.js/auto";
 
 function TotalWeightLifted({ workoutDates, weight }) {
   console.log(Chart);
